8894595f990ca111f3bcf4ddeb9af57494421be7
Eilumial <121159565+Eilumial@users.noreply.github.com>
2023-03-07 09:46:57 +0800

    Add cancel upload function

--- cvparser/src/main/java/com/avensys/cvparser/controller/FileUploadController.java
+++ cvparser/src/main/java/com/avensys/cvparser/controller/FileUploadController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,6 +24,12 @@ public class FileUploadController {
 	@Autowired
 	private FileUploadService fus;
 
+	@GetMapping("/cancel")
+	public ResponseEntity<?> handleFileUpload() {
+		fus.cancelUpload();
+		return ResponseEntity.ok().build();
+	}
+	
 	@PostMapping("/up")
 	public ResponseEntity<?> handleFileUpload(@RequestParam("files") List<MultipartFile> files) {
 		UploadErrorListDTO errors = new UploadErrorListDTO();
--- cvparser/src/main/java/com/avensys/cvparser/service/FileUploadService.java
+++ cvparser/src/main/java/com/avensys/cvparser/service/FileUploadService.java
@@ -205,4 +205,8 @@ public class FileUploadService {
 		}
 	}
 
+	public void cancelUpload() {
+		fileData.clear();
+	}
+
 }
